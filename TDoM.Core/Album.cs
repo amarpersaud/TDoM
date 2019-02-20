d2005d709422df3c601cc59473d948d0ebb1550e
Amar Persaud <tehspartaa@gmail.com>
2019-02-20 03:40:34 -0500

    Documentation of Album class

--- TDoM.Core/Album.cs
+++ TDoM.Core/Album.cs
@@ -8,12 +8,30 @@ namespace TDoM.Core
 {
     public class Album
     {
-
+        /// <summary>
+        /// Artists that worked on the album. May have differing roles from
+        /// </summary>
         public Artist[] AlbumArtists;
 
+        /// <summary>
+        /// Name of the album
+        /// </summary>
         public string Name;
+
+        /// <summary>
+        /// Tracks on the album
+        /// </summary>
         public Track[] Tracks;
 
+        /// <summary>
+        /// Release date of the album
+        /// </summary>
+        public DateTime Release;
+
+        /// <summary>
+        /// Total runtime of all tracks on the album
+        /// </summary>
+        /// <returns></returns>
         public TimeSpan GetRuntime()
         {
             TimeSpan total = new TimeSpan();
@@ -24,6 +42,10 @@ namespace TDoM.Core
             return total;
         }
 
+        /// <summary>
+        /// If the track has a band
+        /// </summary>
+        /// <returns>false if the artists did not form a band, or there is a single artist</returns>
         public bool HasBand()
         {
             foreach(Artist b in AlbumArtists)
@@ -35,6 +57,21 @@ namespace TDoM.Core
             }
             return false;
         }
-
+        /// <summary>
+        /// Gets the band that created the album
+        /// </summary>
+        /// <returns>Band if there is a single band. Null if there is no band or made by a single artist.</returns>
+        public Band GetBand()
+        {
+            foreach (Artist a in AlbumArtists)
+            {
+                if (a is Band)
+                {
+                    return (Band)a;
+                }
+            }
+            //TODO: exeptions and logging
+            return null;
+        }
     }
 }
