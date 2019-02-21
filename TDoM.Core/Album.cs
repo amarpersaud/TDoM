8dd152049a489d21ebb904e9a85926e687c212b5
Amar Persaud <tehspartaa@gmail.com>
2019-02-20 21:43:55 -0500

    Change Album to have same ID as other classes

--- TDoM.Core/Album.cs
+++ TDoM.Core/Album.cs
@@ -14,6 +14,11 @@ namespace TDoM.Core
         public Dictionary<CID, Role[]> AlbumArtists;
 
         /// <summary>
+        /// ID of the album
+        /// </summary>
+        public CID ID;
+
+        /// <summary>
         /// Name of the album
         /// </summary>
         public string Name;
@@ -77,39 +82,6 @@ namespace TDoM.Core
             }
             //TODO: exeptions and logging
             return null;
-        }
-
-        /// <summary>
-        /// ITunes ID. For use in searching for artists or for checking identity
-        /// </summary>
-        public string ITunesID;
-
-        /// <summary>
-        /// All Media Guide (AMG) ID. For use in searching for artists or for checking identity
-        /// </summary>
-        public string AMGID;
-
-        /// <summary>
-        /// Custom TDoM ID. For use in searching for artists or for checking identity. 
-        /// Mainly used in ensuring that artists not on ITunes or AMG still have an identifier.
-        /// </summary>
-        public string TDoMID;
-
-        /// <summary>
-        /// Compares this Artist's id with another to check if they are the same artist
-        /// </summary>
-        /// <param name="a">Other artists</param>
-        /// <returns>False if artist ids are not the same</returns>
-        public bool IsSameAlbum(Album a)
-        {
-            return SameID(this.ITunesID, a.ITunesID) ||
-                   SameID(this.AMGID, a.AMGID) ||
-                   SameID(this.TDoMID, a.TDoMID);
-        }
-
-        private bool SameID(string ida, string idb)
-        {
-            return ida == idb && ida != String.Empty;
         }
     }
 }
